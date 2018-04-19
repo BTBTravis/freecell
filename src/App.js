ccae30626270ce334af99bbe5456af1a0b948ee2
Travis Shears <btbtravis@gmail.com>
2018-04-19 09:36:37 -0500

    Layout dom static structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import createCardAPI from './FreeCell';
+import CardAPI from './FreeCell';
 
 
-const cardAPI = createCardAPI()();
+const cardAPI = new CardAPI();
 console.log('cardAPI: ', cardAPI);
 
+const suits = cardAPI.suits;
+console.log('suits: ', suits);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+  }
+  handleAdd = () => {
+    //this.setState(prevState => ({
+      //isToggleOn: !prevState.isToggleOn
+    //}));
+    console.log("ADD");
+    cardAPI.add();
+    console.log('cardAPI: ', cardAPI);
+    this.setState(prevS => ({
+      count: cardAPI.count
+    }));
+  }
+  componentWillMount() {
+    this.setState(prevS => ({
+      count: cardAPI.count,
+      freeCells: cardAPI.freeCells,
+      scoreCells: cardAPI.scoreCells,
+      cascades: cardAPI.cascades
+    }));
+  }
+  //<button onClick={this.handleAdd}>Add</button>
   render() {
+    const freeCells = this.state.freeCells.map((cell, i) => <div className="cell freecell" key={i}><p className="bgtxt">freecell</p></div>);
+    const scoreCells = this.state.scoreCells.map((cell, i) => <div className="cell scorecell" key={i}><p className="bgtxt">{suits[cell.name].symbol}</p></div>);
+    const cascades = this.state.cascades.map((cas, i) => <Cascade cards={cas} />);
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">FreeCell</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="game-area">
+          <div className="status-bar"><p>10:00 | 3 moves</p></div>
+          <div className="top-field">
+            <div className="freecells">{freeCells}</div>
+            <div id="top-btns">
+              <button>New Game</button>
+              <button>Undo</button>
+            </div>
+            <div className="scorecells">{scoreCells}</div>
+          </div>
+          <div className="main-field">
+            {cascades}
+          </div>
+          <div className="bot-field"></div>
+        </div>
       </div>
     );
   }
+}
+
+function Cascade(props) {
+  console.log('props: ', props);
+  const cards = props.cards.map((card, i) => {
+    const cardStyle = {
+      top: i * 50
+    };
+    const redCard = card.suit == 'hearts' || card.suit == 'diamonds';
+    return <div style={cardStyle} className={"card " + (redCard ? 'red-card' : '')} >
+        <div className="topLeft">
+          <p>{card.name}</p>
+          <p>{suits[card.suit].symbol}</p>
+        </div>
+        <div className="botRight">
+          <p>{suits[card.suit].symbol}</p>
+          <p>{card.name}</p>
+        </div>
+      </div>
+  });
+  console.log('cards: ', cards);
+  return <div className="cascade">{cards}</div>;
 }
 
 export default App;
--- src/FreeCell.js
+++ src/FreeCell.js
@@ -17,76 +17,105 @@ let suits = [
   }
 ];
 
-export default (services) => () => {
-
-  let FreeCells = [[],[],[],[]];
-  let ScoreCells = [[],[],[],[]];
-  // list of all possible cards
-  let cards = [].concat.apply([],
-    suits.map(suit => {
-      let suits_cards = [];
-      for (var i = 0; i < 13; i++) {
-        suits_cards.push({
-          name: i,
-          suit: suit.name
-        });
-      }
-      return suits_cards;
-    })
-  );
-  let cascades = [
-    {
-      cards: [],
-      max: 7
-    },
-    {
-      cards: [],
-      max: 7
-    },
-    {
-      cards: [],
-      max: 7
-    },
-    {
-      cards: [],
-      max: 7
-    },
-    {
-      cards: [],
-      max: 6
-    },
-    {
-      cards: [],
-      max: 6
-    },
-    {
-      cards: [],
-      max: 6
-    },
-    {
-      cards: [],
-      max: 6
-    },
-  ];
-
-  /**
-   * Shuffles array in place.
-   * @param {Array} a items An array containing the items.
-   */
-  let _shuffle = (a) => {
-    for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
+// list of all possible cards
+let cards = [].concat.apply([],
+  suits.map(suit => {
+    let suits_cards = [];
+    for (var i = 0; i < 13; i++) {
+      suits_cards.push({
+        name: i,
+        suit: suit.name
+      });
     }
-    return a;
-  };
+    return suits_cards;
+  })
+);
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+let _shuffle = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+};
+
+export default class CardAPI {
+  constructor() {
+    this.count = 4;
+    this.freeCells = [[],[],[],[]];
+    this.scoreCells = [
+      {
+        name: 'hearts',
+        card:[],
+      },
+      {
+        name: 'clubs',
+        card:[],
+      },
+      {
+        name: 'spades',
+        card:[],
+      },
+      {
+        name: 'diamonds',
+        card:[],
+      }
+    ];
+    this.cascades = [
+      {
+        cards: [],
+        max: 7
+      },
+      {
+        cards: [],
+        max: 7
+      },
+      {
+        cards: [],
+        max: 7
+      },
+      {
+        cards: [],
+        max: 7
+      },
+      {
+        cards: [],
+        max: 6
+      },
+      {
+        cards: [],
+        max: 6
+      },
+      {
+        cards: [],
+        max: 6
+      },
+      {
+        cards: [],
+        max: 6
+      },
+    ];
+    this.cascades =  this._fillcas(this.cascades,_shuffle(cards));
+    this.suits = suits.reduce((carry, suit) => {
+      carry[suit.name] = suit;
+      return carry;
+    }, {});
+  }
+  add() {
+    this.count++;
+  }
+
 
   /**
    * Init fill of cascades with cards
    * @param {Array} expecting sub arrays to be {Object} with .max and .cards
    * @param {Array} cards { .name .suit }
    */
-  let _fillcas = (cas, cards) => {
+  _fillcas (cas, cards) {
     let cardsClone = cards.slice(0);
     let filled = cas.reduce((carry, cas) => {
       let newCas = [];
@@ -98,14 +127,4 @@ export default (services) => () => {
     },{ cards: cardsClone, cas: [] });
     return filled.cas;
   };
-
-
-  return {
-    initCascades: _fillcas(cascades,_shuffle(cards)),
-    shuffledCards: _shuffle(cards)
-  };
-
-
-
-
 }
